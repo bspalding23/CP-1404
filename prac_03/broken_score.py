ca4dafd8e3aebe1cc61597a4f3294d542584ae06
Boyd Spalding <boyd.spalding@my.jcu.edu.au>
2020-03-15 17:32:06 +1000

    Add broken score program  from prac 01 with extra functions implemented

--- prac_03/broken_score.py
+++ prac_03/broken_score.py
@@ -2,12 +2,18 @@
 CP1404/CP5632 - Practical
 Broken program to determine score status
 """
+import random
+
+MIN_SCORE = 0
+MAX_SCORE = 100
 
 
 def main():
     score = float(input("Enter score: "))
-
     print(calculate_result(score))
+
+    random_generated_score = random.randint(MIN_SCORE, MAX_SCORE)
+    print(calculate_result(random_generated_score))
 
 
 def calculate_result(score):
