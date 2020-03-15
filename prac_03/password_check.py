4a6f6135efacc2b65144987e471673fc77f2e509
Boyd Spalding <boyd.spalding@my.jcu.edu.au>
2020-03-15 17:07:41 +1000

    Refactor password check program to use functions

--- prac_03/password_check.py
+++ prac_03/password_check.py
@@ -1,12 +1,25 @@
 
 MIN_LENGTH = 9
 
-password = input("Please enter in a password, it must be at least 9 characters in length\n")
 
-while len(password) < MIN_LENGTH:
-    password = input("Please try again\nPassword was not 9 at least characters in length\n")
+def main():
+    password = get_password()
 
-asterisks = ""
-for char in password:
-    asterisks += "*"
-print(asterisks)
+    print_password(password)
+
+
+def print_password(password):
+    asterisks = ""
+    for char in password:
+        asterisks += "*"
+    print(asterisks)
+
+
+def get_password():
+    password = input("Please enter in a password, it must be at least 9 characters in length\n")
+    while len(password) < MIN_LENGTH:
+        password = input("Please try again\nPassword was not 9 at least characters in length\n")
+    return password
+
+
+main()
