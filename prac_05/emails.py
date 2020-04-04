5c1c70d807f60beaaa751f08e4c400ea5777573e
Boyd Spalding <boyd.spalding@my.jcu.edu.au>
2020-04-04 12:47:03 +1000

    Add finished code to emails

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -1,7 +1,30 @@
 
+QUIT = ""
+YES = "Y"
+DEFAULT_ACCEPT = ""
+
 
 def main():
-    print()
+    email_to_name = {}
+    user_email = input("Please enter a email to store:\n")
+    while user_email != QUIT:
+        name = get_name_from_email(user_email)
+        conformation = input("Is {} your name? (Y/N)\n".format(name)).upper()
+        if conformation != YES and conformation != DEFAULT_ACCEPT:
+            name = input("Please enter name:\n")
+        email_to_name[user_email] = name
+
+        user_email = input("Please enter a email to store:\n")
+
+    for email, name in email_to_name.items():
+        print("{} ({})".format(name, email))
+
+
+def get_name_from_email(user_email):
+    prefix = user_email.split("@")[0]
+    name_parts = prefix.split(".")
+    name = " ".join(name_parts).title()
+    return name
 
 
 main()
